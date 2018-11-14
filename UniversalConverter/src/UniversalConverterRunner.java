f545a9ff7f8ba54cb0fbb3ba26c8c107a662bc04
joshua.obrecht <joshua.obrecht@MU-C1-11.mullen.pvt.k12.co.us>
2018-11-14 15:00:41 -0700

    worked on temperature conversions

--- UniversalConverter/src/UniversalConverterRunner.java
+++ UniversalConverter/src/UniversalConverterRunner.java
@@ -4,9 +4,13 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import javafx.scene.layout.Border;
+
 public class UniversalConverterRunner
 	{
 
+		static JFrame frame = new JFrame("Universal Converter v.1");
+		static JPanel panel = new JPanel();
 		public static void main(String[] args)
 			{
 			displayJFrame();
@@ -14,12 +18,12 @@ public class UniversalConverterRunner
 			}
 		public static void displayJFrame()
 		{
-			JFrame frame = new JFrame("Universal Converter v.1");
+			
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setSize(300, 150);
 			frame.setLocation(430, 100);
 			
-			JPanel panel = new JPanel();
+			
 			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 			frame.add(panel);
 			
@@ -60,7 +64,52 @@ public class UniversalConverterRunner
 		}
 		public static void convertTemp()
 		{
-			System.out.println("test");
+			frame.getContentPane().removeAll();
+			frame.repaint();
+			frame.getContentPane().setLayout(new FlowLayout());
+			
+			JLabel lbl = new JLabel("Type in your current temperature. Select what you would like to convert it to.");
+			lbl.setAlignmentX(Component.CENTER_ALIGNMENT);
+			frame.add(lbl);
+			
+			String [] temperatures  = {"Farenheit", "Celsius"};
+			final JComboBox<String> cb = new JComboBox<String>(temperatures);
+			cb.setAlignmentX(Component.CENTER_ALIGNMENT);
+			frame.add(cb);
+			
+			JTextField f1= new JTextField("Temperature 1", 10);
+			JTextField f2= new JTextField("Converted Temp", 10);
+			f2.setLocation(250, 125);
+			
+			frame.getContentPane().add(f1);
+			frame.getContentPane().add(f2);
+			
+			JButton btn = new JButton("CONVERT");
+			btn.setAlignmentX(Component.CENTER_ALIGNMENT);
+			frame.add(btn);
+			
+			btn.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+						switch(cb.getSelectedIndex())
+						{
+							case 0:
+								String userValue = f1.getText();
+								double userTemp = Double.parseDouble(userValue);
+								System.out.println((userTemp*(9/5))+32);
+								break;
+							case 1:
+								System.out.println("yo");
+						}
+					}
+				});
+			
+			
+			
+			frame.pack();
+			frame.setSize(500, 150);
+			panel.setVisible(true);
 		}
 
 	}
