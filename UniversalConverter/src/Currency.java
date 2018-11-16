e338528057dcf9882a836068cafbcdb0ede46cdc
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-11-16 14:51:51 -0700

    dsfiopp

--- UniversalConverter/src/Currency.java
+++ UniversalConverter/src/Currency.java
@@ -1,5 +1,18 @@
 
 public class Currency
 	{
-		
+		public static void convert$()
+			{
+				//US
+				double USD = 1;
+				//European Union 0.88
+				double euro = 1;
+				//China 6.94
+				double yuan = 1;
+				//Japan 112.87
+				double yen = 1;
+				//uk 0.78
+				double pound = 1;
+				
+			}
 	}
--- UniversalConverter/src/UniversalConverterRunner.java
+++ UniversalConverter/src/UniversalConverterRunner.java
@@ -9,9 +9,9 @@ public class UniversalConverterRunner
 
 		public static void main(String[] args)
 			{
-			displayJFrame();
-				//WASSUUUUUUPPPP MY BROSKIIIIIIIIII
-			//idehuioasebhdfb
+				
+				displayJFrame();
+		
 			}
 		public static void displayJFrame()
 		{
@@ -55,10 +55,14 @@ public class UniversalConverterRunner
 								break;
 							case 1:
 								System.out.println("yo");
+							case 2:
+							
 						}
 					}
-				});
+				}
+			);
 		}
+		
 		public static void convertTemp()
 		{
 			System.out.println("test");
